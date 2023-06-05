c721668a2860d9f7c330850f723efd59aacc0bd2
carlos51 <carlos@live.com>
2023-06-05 11:25:57 -0600

    correccion cuda

--- CPU/calor2dcuda.py
+++ CPU/calor2dcuda.py
@@ -13,7 +13,7 @@ n = np.array([512,512])
 # Tamaño del dominio (menor que uno)
 L =np.array([1.0,1.0])
 # Constante de difusión
-k = 0.2
+kd = 0.2
 # Pasos de tiempo
 pasos = 1000
 
@@ -22,23 +22,23 @@ pasos = 1000
 dx = L/n
 udx2 = 1.0/(dx*dx)
 # Paso de tiempo
-dt = 0.25*(min(dx[0],dx[1])**2)/k
+dt = 0.25*(min(dx[0],dx[1])**2)/kd
 print("dt = ", dt)
 # Total de celdas
 nt = n[0]*n[1]
 print("celdas = ", nt)
 
 @jit
-def evolucion(u,n,udx2,dt,i,k):
-    jp1 = i + n[0]
-    jm1 = i - n[0]
+def evolucion(u,n_0,n_1,udx2_0,udx2_1,dt,kd,i):
+    jp1 = i + n_0
+    jm1 = i - n_1
     laplaciano = (u[i-1]-2.0*u[i]+u[i+1])*udx2[0] + (u[jm1]-2.0*u[i]+u[jp1])*udx2[1]
-    unueva = u[i] + dt*k*laplaciano
+    unueva = u[i] + dt*kd*laplaciano
     return unueva
 evolucion_gpu = cuda.jit(device=True)(evolucion)
 
 @cuda.jit
-def solucion_kernel(u_d,un_d,udx2_0,udx_1,dt,n_1,kd):
+def solucion_kernel(u_d,un_d,udx2_0,udx2_1,dt,n_0,n_1,kd):
     ii, jj = cuda.grid(2)
     i = ii + n_0*jj
     if ii == 0 or jj ==0 or ii == n_0-1 or jj == n_1-1:
@@ -67,7 +67,7 @@ x,y = np.meshgrid(np.arange(0,L[0],dx[0]),np.arange(0,L[1],dx[1]))
 ax = plt.axes(projection = "3d")
 star = time.time()
 for t in range(1,pasos+1):
-    solucion_kernel[griddim,blockdim](u_d,un_d,udx2[0],udx2[1],dt,n[0].n[1],kd)
+    solucion_kernel[griddim,blockdim](u_d,un_d,udx2[0],udx2[1],dt,n[0],n[1],kd)
     u_d = cuda.to_device(un_d)
     if t%100==0: print("Iteracion = ", t)
 # Pasar arreglo al CPU
@@ -77,3 +77,4 @@ end = time.time()
 u = np.reshape(u,(n[0],n[1]))
 ax.plot_surface(x,y,u,cmap=cm.hsv)
 plt.show()
+
